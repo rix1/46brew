00ee48d640ebf3dc6d18ccd379d42f28a6b5728d
Rikard Eide <rikardeide@gmail.com>
2018-08-25 16:28:10 +0200

    Emotion: Add css-in-js library Emotion

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,14 @@
-// @flow
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import { extractCritical } from 'emotion-server';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const page = renderPage();
+    const styles = extractCritical(page.html);
+    return { ...page, ...styles };
+  }
+
   render() {
     return (
       <html lang="en">
@@ -10,11 +16,11 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-
           <link
             rel="stylesheet"
             href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"
           />
+          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
         </Head>
         <body className="sans-serif" style={{ fontSize: '18px' }}>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 // @flow
 import React from 'react';
+import styled, { hydrate, css } from 'react-emotion';
 import Page from '../components/Page';
 import Content from '../components/Content';
+
+// Adds server generated styles to emotion cache.
+// '__NEXT_DATA__.ids' is set in '_document.js'
+// if (typeof window !== 'undefined') {
+//   hydrate(window.__NEXT_DATA__.ids); // eslint-disable-line
+// }
 
 export default () => (
   <Page>
     <Content>
-      <div>Hello World</div>
+      <div
+        className={css`
+          background: hotpink;
+          &:hover {
+            background: #ffff00;
+          }
+        `}>
+        Hello World
+      </div>
     </Content>
   </Page>
 );
