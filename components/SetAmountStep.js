d5b4cd4ab7eb192382df962d12b48726c5f307d6
Rikard Eide <rikardeide@gmail.com>
2018-09-06 23:17:33 +0200

    firstStep: add conversion between coffe, water and cups

--- components/SetAmountStep.js
+++ components/SetAmountStep.js
@@ -1,6 +1,9 @@
 // @flow
 import React, { Fragment, PureComponent } from 'react';
 
+import { coffeeConverter, coffeeToWater } from '../lib/conversion';
+
+import { COFFE_CUP_SIZE } from '../lib/constants';
 import InlineInput from './InlineInput';
 import ColorButton from './ColorButton';
 
@@ -40,27 +43,46 @@ class SetAmountStep extends PureComponent<Props, State> {
     if (!Number.isNaN(Number(value))) {
       this.setState({
         value,
-        type: Number(value) > 100 ? 'water' : 'coffee',
+        type: this.getType(value),
         inputError: false,
       });
     }
   };
 
+  getType = (value: number | string) =>
+    Number(value) > 100 ? 'water' : 'coffee';
+
+  getWater = () => {
+    const { value, type } = this.state;
+    if (type === 'water') {
+      return Number(value);
+    }
+    return coffeeToWater(Number(value));
+  };
+
   render() {
     const { value, type, inputError } = this.state;
+    const converted = (type && coffeeConverter(Number(value), type)) || 0;
+    const cups = Math.round(Number(this.getWater() / COFFE_CUP_SIZE) * 10) / 10;
+
     return (
       <Fragment>
         <p className="f3">
           Please let me know how much coffee (or water) you are going to use:
           <InlineInput
-            type="text"
+            type="number"
             className="ph1"
             value={value}
             onChange={this.handleChange}
             error={inputError}
           />{' '}
-          g{type && ` of ${type}`}.
+          g{type && ` of ${type}.`}
         </p>
+        <span className="db lh-copy fw5">That is...</span>
+        <span className="ml3 db lh-copy">
+          {converted} grams of {this.getType(converted)}
+        </span>
+        <span className="ml3 db mb3 lh-copy">{cups} cups of coffee</span>
         <ColorButton onClick={this.completeStep}>Next</ColorButton>
       </Fragment>
     );
