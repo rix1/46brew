9bc8cd1cf5ecab11c9d4d30ca76d06f24abc0108
Rikard Eide <rikardeide@gmail.com>
2019-09-19 10:46:33 +0200

    Next: Remove deprecated <Container /> in App

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 
 import Navigation from '../components/Navigation';
 import Link from '../components/Link';
@@ -8,25 +8,23 @@ export default class MyApp extends App {
   render() {
     const { Component } = this.props;
     return (
-      <Container>
-        <div className="flex flex-column vh-100">
-          <header className="tc">
-            <h1 className="f1 fw7 ttu tracked relative dib">4:6brew</h1>
-            <Navigation />
-          </header>
-          <div>
-            <Component />
-          </div>
-
-          <footer className="tc f6 moon-gray mb3 mt7">
-            Created by{' '}
-            <Link className="underline silver" href="https://twitter.com/rix1">
-              rix1
-            </Link>{' '}
-            a weekend in August 2018
-          </footer>
+      <div className="flex flex-column vh-100">
+        <header className="tc">
+          <h1 className="f1 fw7 ttu tracked relative dib">4:6brew</h1>
+          <Navigation />
+        </header>
+        <div>
+          <Component />
         </div>
-      </Container>
+
+        <footer className="tc f6 moon-gray mb3 mt7">
+          Created by{' '}
+          <Link className="underline silver" href="https://twitter.com/rix1">
+            rix1
+          </Link>{' '}
+          a weekend in August 2018
+        </footer>
+      </div>
     );
   }
 }
