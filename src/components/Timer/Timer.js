3c9b4cf42c54d32201c58db20279e1a16871eb93
Rikard Eide <rikardeide@gmail.com>
2019-12-11 17:44:18 +0200

    Timer: Add multiplier prop to contextProvider

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -3,18 +3,24 @@ import React, { useContext } from 'react';
 import { useTimer } from './useTimer';
 import TimerContext from './TimerContext';
 
-type TimerContextProviderProps = {
+type TimerContextProviderProps = {|
   children: React$Node,
-};
+  multiplier?: number,
+|};
 
 export const TimerContextProvider = ({
   children,
+  multiplier,
 }: TimerContextProviderProps) => {
-  const { time, isRunning, toggleTimer, reset } = useTimer();
+  const { time, isRunning, toggleTimer, reset } = useTimer(multiplier);
   const context = { time, isRunning, toggleTimer, reset };
   return (
     <TimerContext.Provider value={context}>{children}</TimerContext.Provider>
   );
+};
+
+TimerContextProvider.defaultProps = {
+  multiplier: undefined,
 };
 
 export function useTimerContext() {
