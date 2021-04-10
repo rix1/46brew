a3144e2bcff0966668684d6c8ba2d8ca924856db
Rikard Eide <rikardeide@gmail.com>
2021-04-10 20:33:01 +0200

    next-offline: remove service-worker setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const withOffline = require('next-offline');
 const pkg = require('./package.json');
 
-const baseConfig = {
+module.exports = {
   env: {
     NAME: pkg.name,
     ENV: process.env.NODE_ENV,
@@ -9,40 +8,3 @@ const baseConfig = {
     VERCEL_GITHUB_COMMIT_SHA: process.env.VERCEL_GITHUB_COMMIT_SHA,
   },
 };
-
-const serviceWorkerConfig = {
-  target: 'serverless',
-  transformManifest: (manifest) => ['/'].concat(manifest), // add the homepage to the cache
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
-  generateInDevMode: true,
-  workboxOpts: {
-    modifyURLPrefix: {
-      'static/': '_next/static/',
-      'public/': '/',
-    },
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
-};
-
-module.exports =
-  process.env.NODE_ENV === 'development'
-    ? baseConfig
-    : withOffline({ ...baseConfig, ...serviceWorkerConfig });
